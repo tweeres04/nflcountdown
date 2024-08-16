606d920bb24d1cad712cacfb2fb3c9d72972bfac
Tyler Weeres <tweeres04@gmail.com>
2024-08-16 06:44:10 -0700

    Support NHL countdown

--- scripts/takeCountdownScreenshots.ts
+++ scripts/takeCountdownScreenshots.ts
@@ -1,14 +1,18 @@
 import puppeteer from 'puppeteer'
-import { uniqBy } from 'lodash-es'
+import { uniqBy, kebabCase } from 'lodash-es'
+
 import { mlbTeamToTeam } from '~/lib/mlbGameToGame'
 
 import schedule from '../nfl_schedule.json'
 import mlbTeams from '../mlb_teams.json'
+import { teams as nhlTeams } from '../nhl_teams'
 
 const LEAGUE = (process.env.LEAGUE ?? 'NFL').toLowerCase()
 let teams =
 	LEAGUE === 'mlb'
 		? mlbTeams.teams.map(mlbTeamToTeam)
+		: LEAGUE === 'nhl'
+		? nhlTeams
 		: uniqBy(
 				schedule.games.map((g) => g.homeTeam),
 				'id'
@@ -18,13 +22,20 @@ const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
 await teams.reduce(async (p, team) => {
+	const slug =
+		LEAGUE === 'nhl'
+			? kebabCase(team.teamName)
+			: team.abbreviation.toLowerCase()
+	const pageUrl = `http://localhost:3000/${slug}${
+		LEAGUE === 'nhl' ? '/countdown' : ''
+	}`
 	await p
-	await page.goto(`http://localhost:5173/${team.abbreviation.toLowerCase()}`, {
+	await page.goto(pageUrl, {
 		waitUntil: 'networkidle2',
 	})
 	await page.setViewport({ width: 1920, height: 1080 })
 
-	const fileElement = await page.waitForSelector('div.countdown')
+	const fileElement = await page.waitForSelector('div#countdown')
 	const filename = `public/og/${LEAGUE}/${team.abbreviation.toLowerCase()}.png`
 	await fileElement?.screenshot({
 		path: filename,
