134e6f3905078586df090ad14b9bbddeb2e59b06
Tyler Weeres <tweeres04@gmail.com>
2024-08-29 08:14:18 -0700

    Put sitemap in the normal place

--- app/routes/robots[.]txt.ts
+++ app/routes/robots[.]txt.ts
@@ -4,7 +4,7 @@ export async function loader() {
 	const body = `User-Agent: *
 Disallow:
 
-Sitemap: https://${LEAGUE}countdown.tweeres.com/sitemap`
+Sitemap: https://${LEAGUE}countdown.tweeres.com/sitemap.xml`
 
 	return new Response(body, {
 		headers: {
