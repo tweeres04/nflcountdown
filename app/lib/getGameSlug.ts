91501d5702b0aa3c914561965dd029b744056b0e
Tyler Weeres <tweeres04@gmail.com>
2024-11-28 14:27:07 -0800

    Use date-fns parseISO to try to get dates in proper TZ

--- app/lib/getGameSlug.ts
+++ app/lib/getGameSlug.ts
@@ -1,8 +1,9 @@
 import { Game } from './types'
+import { parseISO } from 'date-fns/parseISO'
 
 export function getGameSlug(game: Game, teamAbbrev: string) {
 	if (!game.time) return null
-	const date = new Date(game.time)
+	const date = parseISO(game.time)
 	const month = date.toLocaleString('default', { month: 'short' }).toLowerCase()
 	const day = date.getDate()
 	const year = date.getFullYear()
