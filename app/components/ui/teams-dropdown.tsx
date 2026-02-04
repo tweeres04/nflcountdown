27d48bbdc9d5618a7a93124b23f058e2ff1169a4
Tyler Weeres <tweeres04@gmail.com>
2026-02-04 09:52:54 -0800

    Hidden nav for SEO

--- app/components/ui/teams-dropdown.tsx
+++ app/components/ui/teams-dropdown.tsx
@@ -33,38 +33,61 @@ export default function TeamsDropdown({
 	const contentClasses = `text-white bg-${color} hover:bg-${color} hover:text-white border-0 rounded-none`
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-			<DropdownMenuContent className={contentClasses}>
-				{LEAGUE === 'NFL' ? (
-					<DropdownMenuItem asChild>
-						<a href="/nfl/season">2026 NFL Season</a>
-					</DropdownMenuItem>
-				) : null}
-			<DropdownMenuSub>
-				<DropdownMenuSubTrigger
-					className={`data-[state=open]:bg-${color} focus:bg-${color}`}
-				>
-					More leagues
-				</DropdownMenuSubTrigger>
-				<DropdownMenuSubContent className={contentClasses}>
-					{['NFL', 'MLB', 'NBA', 'WNBA', 'NHL', 'MLS'].map((league) =>
-						LEAGUE !== league ? (
-							<DropdownMenuItem asChild key={league}>
-								<a href={`/${league.toLowerCase()}`}>{league}</a>
-							</DropdownMenuItem>
-						) : null
-					)}
-				</DropdownMenuSubContent>
-			</DropdownMenuSub>
+		<>
+			{/* Hidden navigation for SEO - includes all team and league links */}
+			<nav className="sr-only" aria-hidden="true">
+				{LEAGUE === 'NFL' && <a href="/nfl/season">2026 NFL Season</a>}
 				{teams.map((t) => (
-					<DropdownMenuItem asChild key={t.abbreviation}>
-						<a href={`/${lowercaseLeague}/${t.abbreviation.toLowerCase()}`}>
-							{t.fullName}
-						</a>
-					</DropdownMenuItem>
+					<a
+						key={t.abbreviation}
+						href={`/${lowercaseLeague}/${t.abbreviation.toLowerCase()}`}
+					>
+						{t.fullName}
+					</a>
 				))}
-			</DropdownMenuContent>
-		</DropdownMenu>
+				{['NFL', 'MLB', 'NBA', 'WNBA', 'NHL', 'MLS'].map((league) =>
+					LEAGUE !== league ? (
+						<a key={league} href={`/${league.toLowerCase()}`}>
+							{league}
+						</a>
+					) : null
+				)}
+			</nav>
+
+			{/* Visible dropdown menu for users */}
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+				<DropdownMenuContent className={contentClasses} forceMount>
+					{LEAGUE === 'NFL' ? (
+						<DropdownMenuItem asChild>
+							<a href="/nfl/season">2026 NFL Season</a>
+						</DropdownMenuItem>
+					) : null}
+					<DropdownMenuSub>
+						<DropdownMenuSubTrigger
+							className={`data-[state=open]:bg-${color} focus:bg-${color}`}
+						>
+							More leagues
+						</DropdownMenuSubTrigger>
+						<DropdownMenuSubContent className={contentClasses} forceMount>
+							{['NFL', 'MLB', 'NBA', 'WNBA', 'NHL', 'MLS'].map((league) =>
+								LEAGUE !== league ? (
+									<DropdownMenuItem asChild key={league}>
+										<a href={`/${league.toLowerCase()}`}>{league}</a>
+									</DropdownMenuItem>
+								) : null
+							)}
+						</DropdownMenuSubContent>
+					</DropdownMenuSub>
+					{teams.map((t) => (
+						<DropdownMenuItem asChild key={t.abbreviation}>
+							<a href={`/${lowercaseLeague}/${t.abbreviation.toLowerCase()}`}>
+								{t.fullName}
+							</a>
+						</DropdownMenuItem>
+					))}
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</>
 	)
 }
